08fbceabeed7c54e62fa1a8880e24c0a53d37555
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 16:19:13 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -3,11 +3,14 @@ class Song
   @@all = []
   def initialize(name, artist="nil")
     @name = name
-    @artist = artist if artist != nil
+    self.artist=(artist) if artist != nil
   end
 
-
-
+  def artist=(artist)
+    @artist = artist 
+    artist.add_song(self)
+  end 
+  
   def self.all
     @@all
   end
