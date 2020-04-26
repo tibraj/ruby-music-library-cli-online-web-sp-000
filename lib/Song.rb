9bf3ec8485cb2aa805e31fa81145f5f698831ada
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 17:07:15 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -48,4 +48,8 @@ class Song
     @@all << song
     song
   end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) == nil ? self.create_by_name(song_name) : self.find_by_name(song_name)
+  end
 end
