de1463b8aabd18d30b616d98bb36169c199c666c
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 17:04:09 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -38,8 +38,8 @@ class Song
     song
   end
 
-  def self.find_by_name(song)
-    @@all.find {|song| song.name == song}
+  def self.find_by_name(song_name)
+    @@all.find {|song| song.name == song_name}
   end
 
 
