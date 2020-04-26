58aa8d36f4245982c0fa78056b3db7df02f9ca9c
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 16:45:42 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -11,6 +11,9 @@ class Artist
     @songs << song unless @songs.include?(song)
   end
 
+  def genres
+    @artist_genres = []
+    @songs.collect {|song| @artist_genres << song.genre unless @artist_genres.include?(song.genre) }
 
   def self.all
     @@all
