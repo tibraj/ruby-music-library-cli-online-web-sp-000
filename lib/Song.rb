9f14d5bee954dc7c05763071bd28e458c9308951
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 17:23:35 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -45,4 +45,15 @@ class Song
   def self.find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create(song_name)
   end
+
+  def self.new_from_filename(filename)
+    song = self.new(filename)
+    song.name = (name.split(" - ")[1].chomp(.".mp3"))
+    song
+  end 
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename)
+    @@all << song 
+  end 
 end
