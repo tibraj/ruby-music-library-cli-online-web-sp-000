a9aceaa0f226494599d03c44dce1c85a0a75952b
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 16:42:22 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -4,7 +4,7 @@ class Song
   def initialize(name, artist=nil, genre=nil)
     @name = name
     self.artist=(artist) if artist != nil
-    self.genre=(genre) if genre != nil 
+    self.genre=(genre) if genre != nil
   end
 
   def artist=(artist)
