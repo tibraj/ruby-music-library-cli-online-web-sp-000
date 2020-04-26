13d4af6439f872b289750f28b76cc51e86517356
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 16:21:33 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist
   @@all = []
-  def initialize(name, artist=nil)
+  def initialize(name, artist=nil, genre=nil)
     @name = name
     self.artist=(artist) if artist != nil
   end
