a09639e43254f3dc5391ee9105a7270f9a8c9fa1
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 17:14:41 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,4 +2,8 @@ module Concerns::Findable
   def find_by_name(name)
     self.all.find {|i| i.name == name}
   end
+
+  def find_or_create_by_name(name)
+    self.all.find_by_name(name) || self.all.create(name)
+  end
 end
