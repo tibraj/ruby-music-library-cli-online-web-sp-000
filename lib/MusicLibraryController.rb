400cd211bf329a01375bf05eb2857765ef5a5bcb
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 17:32:11 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,7 +1,13 @@
 class MusicLibraryController
   attr_accessor :path
+
   def initialize(path='./db/mp3s')
   @path = path
   MusicImporter.new(path).import
+  end
+
+  def call
+    puts "Welcome!"
+
   end
 end
