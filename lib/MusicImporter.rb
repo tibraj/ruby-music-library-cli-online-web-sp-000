c4831a0d3f913178d3a155e49ca13f44e885b706
Taulant Ibraj <tibraj94@gmail.com>
2020-04-26 17:20:39 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -4,4 +4,11 @@ class MusicImporter
     @path = path
   end
 
-end 
+  def files
+    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
+  end
+
+  def import
+    self.files.each {|filename| Song.create_from_filename(filename)}
+  end
+end
